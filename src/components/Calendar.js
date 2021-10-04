c08773830f0c3b4de2054661200c9b74b5aa76dc
Nedim <mr.ramicevic@gmail.com>
2021-10-04 09:40:04 +0300

    init

---
+++ src/components/Calendar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Day from './Day'
+
+function Calendar() {
+    
+    return (
+        <div className="calendar">
+            {
+                <Day/>
+            }
+        </div>
+    )
+}
+
+export default Calendar
---
+++ src/components/Day.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Day() {
+    return (
+        <div className="day">
+            selam
+        </div>
+    )
+}
+
+export default Day
