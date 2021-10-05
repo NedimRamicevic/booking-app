e68d42d2c17f92fddc2d49f09c8afe8e6136a503
Nedim <mr.ramicevic@gmail.com>
2021-10-05 12:38:02 +0300

    schedule1

--- src/components/schedule/Oda.js
+++ src/components/schedule/Oda.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
-export default function Oda() {
-    
+export default function Oda({oda}) {
+    var list = [1,2,3,4,5,6,7]
+    const selectedDays = [1,7]
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    console.log(oda)
     return (
         <div className="oda">
-            <text>{today.getDay()}</text>
+            <p>{`Oda ${oda.odaId}`} </p>
+            {oda ? (
+                list.map(x =>
+                        {return  (new Date(oda.gün1)).getDate() <= x & x <=  (new Date(oda.gün2)).getDate()  ?(
+                            <div className="odagün">
+                            <h10>{`${oda.kullanıcı1} - ${oda.kullanıcı2}`}</h10>
+                            <h11>{`${(new Date(oda.gün1)).getDate()} ${monthNames[(new Date(oda.gün1)).getMonth()]} -${(new Date(oda.gün2)).getDate()} ${monthNames[(new Date(oda.gün2)).getMonth()]} `}</h11>
+                            </div>
+                        ):
+                        <text>boş</text>
+                        }
+
+                    )
+            ):null
+            }
         </div>
     )
 }
--- src/components/schedule/Odalar.js
+++ src/components/schedule/Odalar.js
@@ -1,10 +1,33 @@
 import React,{useState,useEffect} from 'react'
+import Oda from './Oda'
 
 function Odalar() {
         
         var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         const [list2, setList2] = useState()
-        const [odalar, setodalar] = useState()
+        const [odalar, setodalar] = useState([
+            {
+                odaId : 1,
+                kullanıcı1 : "Nedim",
+                kullanıcı2 : "Neim",
+                gün1 : "9/01/2021",
+                gün2 : "9/3/2021" 
+            },
+            {
+                odaId : 2,
+                kullanıcı1 : "Nedim",
+                kullanıcı2 : "Neim",
+                gün1 : "9/01/2021",
+                gün2 : "9/3/2021" 
+            },
+            {
+                odaId : 3,
+                kullanıcı1 : "Nedim",
+                kullanıcı2 : "Neim",
+                gün1 : "9/01/2021",
+                gün2 : "9/3/2021" 
+            }
+        ])
         useEffect(() => {
             let list1 = []
             for (let index = 1; index <= 7; index++) {  
@@ -26,6 +49,13 @@ function Odalar() {
                         <button>{x.day}{"\n"}{x.dayName}</button>
                         )
                 ):null}
+                </div>
+                <div className="odalars">
+                        {odalar ? (
+                            odalar.map(oda=>
+                                <Oda oda = {oda} ></Oda>
+                                )
+                        ):null}
                 </div>
             </div>
         )
