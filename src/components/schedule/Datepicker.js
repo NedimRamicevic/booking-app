a5c0f2a9f243663382dea46c52b30868a8bf80e6
Nedim <mr.ramicevic@gmail.com>
2021-10-18 10:57:48 +0300

    datePickerDone

--- src/components/schedule/Datepicker.js
+++ src/components/schedule/Datepicker.js
@@ -18,6 +18,7 @@ function Datepicker() {
     x.setDate(x.getDate() +6)
 
     var aySecildi = (girdi) =>{
+      console.log("nabeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeer")
       //buraya ayı seçince hangi aysa o ayın ilk gününü dateIntervale verirsin. Aşagıdakini dinamik yaparsın
       // her seçilen ay için değişince odalar da otomatik değişir.
       setdateInterval(new Date(`${girdi}/1/2021`))
@@ -27,6 +28,7 @@ function Datepicker() {
       ))
   }
   var dateUp = () =>{
+    console.log("nabeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeer")
         let x = dateInterval
         x.setDate(x.getDate() +7)
         setdateInterval(new Date(`${x.getMonth() + 1}/${x.getDate()}/2021`))
@@ -35,6 +37,7 @@ function Datepicker() {
         ))
     }
   var dateDown = () => {
+    console.log("nabeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeer")
         let x = dateInterval
         x.setDate(x.getDate() - 7)
         setdateInterval(new Date(`${x.getMonth() + 1}/${x.getDate()}/2021`))
@@ -54,8 +57,8 @@ function Datepicker() {
             <text>{interval.getDate()} {monthsList[interval.getMonth()]} - {x.getDate()} {monthsList[x.getMonth()]}</text>
         </div>
         <div className="weekintervalpicker">
-            <button onClick={() => dateDown}>{"<"}</button>
-            <button onClick={() => dateUp}>{">"}</button>
+            <button onClick={dateDown}>{"<"}</button>
+            <button onClick={dateUp}>{">"}</button>
         </div>
   <div className="dropdown">
 
